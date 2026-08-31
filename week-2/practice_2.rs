0b56f9efe732f63077d3390263cbc4dcc93504a0
pioikechukwu1@gmail.com <pioikechukwu@gmail.com>
2026-08-31 17:37:43 +0100

    First commit in week 2

---
+++ week-2/practice_2.rs
@@ -0,0 +1,6 @@
+fn main(){
+	println!();
+	println!("hello");
+	println!("format {} arguments", "some");
+	println!("My name is {}. I am a member of the Computer Science department", "Pio Ike-Omeje")
+}
